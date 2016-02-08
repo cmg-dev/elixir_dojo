a217058826d5ec4597524fd3602e097412ea022e
Christoph <Christoph.Gnip@gmail.com>
2016-02-08 21:52:50 +0100

    Update and pretify

--- github_issue_parser/test/cli_test.exs
+++ github_issue_parser/test/cli_test.exs
@@ -1,16 +1,17 @@
 defmodule CliTest do
   use ExUnit.Case
 
-
-  #dasda
   import GithubIssueParser.CLI, only: [ sort_descending: 1, convert_to_list_of_hashdicts: 1 ]
-  test "test_name" do
+
+  test "Sort 1, 3, 2 to 1, 2, 3" do
     the_list = create_fake_at_list([1, 3, 2])
     the_expectes_list = create_fake_at_list([1, 2, 3])
     assert sort_descending(the_list) == the_expectes_list
   end
 
-  defp create_fake_at_list(list_of_values) do
+  @doc """
+    This function mocks the list we expect as HTTP response.
+  """defp create_fake_at_list(list_of_values) do
     data = for value <- list_of_values,
                 do: [{"created_at", value}, {"dont_care", "xxx"}]
     convert_to_list_of_hashdicts( data )
