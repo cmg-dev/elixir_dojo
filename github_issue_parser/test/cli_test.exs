60ca09c075d1f20989021aebd267c49bcbe29a46
Christoph <Christoph.Gnip@gmail.com>
2016-02-08 22:45:27 +0100

    Fix error; Small reformatting

--- github_issue_parser/test/cli_test.exs
+++ github_issue_parser/test/cli_test.exs
@@ -1,7 +1,9 @@
 defmodule CliTest do
   use ExUnit.Case
 
-  import GithubIssueParser.CLI, only: [ sort_descending: 1, convert_to_list_of_hashdicts: 1 ]
+  import GithubIssueParser.CLI, only:
+        [ sort_descending: 1,
+          convert_to_list_of_hashdicts: 1 ]
 
   test "Sort 1, 3, 2 to 1, 2, 3" do
     the_list = create_fake_at_list([1, 3, 2])
@@ -11,7 +13,8 @@ defmodule CliTest do
 
   @doc """
     This function mocks the list we expect as HTTP response.
-  """defp create_fake_at_list(list_of_values) do
+  """
+  defp create_fake_at_list(list_of_values) do
     data = for value <- list_of_values,
                 do: [{"created_at", value}, {"dont_care", "xxx"}]
     convert_to_list_of_hashdicts( data )
