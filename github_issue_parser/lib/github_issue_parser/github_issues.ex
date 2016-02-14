5500d5eed238060cda5ac7944faae0ae01a3eca8
Christoph <Christoph.Gnip@gmail.com>
2016-02-14 21:20:56 +0100

    Fix the wrong URL bug; Fix github URL
    This fixes #4, #5.
    
    A bug in the sample config is now fixed. The github url is now back to
    normal. And the test runs fine.

--- github_issue_parser/lib/github_issue_parser/github_issues.ex
+++ github_issue_parser/lib/github_issue_parser/github_issues.ex
@@ -1,6 +1,6 @@
 defmodule GithubIssueParser.GithubIssues do
-  @github_url Application.get(:issues, :github_url)_
-  @access_token Application.get(:api, :access_token)_
+  @github_url Application.get_env(:issues, :github_url)
+  @access_token Application.get_env(:api, :access_token)
 
   @user_agent [{ "User-agent", "RequestIssues christoph.gnip@gmail.com"}]
 
@@ -11,7 +11,7 @@ defmodule GithubIssueParser.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "#{@github_url}#{user}/#{project}/issues?access_token=#{@access_token}"
+    "#{@github_url}/repos/#{user}/#{project}/issues?access_token=#{@access_token}"
   end
 
   @doc """
@@ -25,6 +25,7 @@ defmodule GithubIssueParser.GithubIssues do
   This handles every other status code, which will be an error for us.
   """
   def handle_response({:ok, %HTTPoison.Response{status_code: ___, body: body}}) do
+    IO.puts "response was :error #{body}"
     { :error, :jsx.decode(body) }
   end
 end
