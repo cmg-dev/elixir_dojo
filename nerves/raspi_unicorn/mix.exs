83a48a9243e05878687ae02cbdfc36fe4286976f
Christoph <Christoph.Gnip@gmail.com>
2016-02-19 20:34:45 +0100

    Fill the project with life
    The basic functionality is derived from the nerves-example project
    blinky. Minor adjustments (like renaming) has been done and it compiles
    just fine.

---
+++ nerves/raspi_unicorn/mix.exs
@@ -0,0 +1,24 @@
+defmodule Blinky.Mixfile do
+
+  use Mix.Project
+
+  def project do
+    [app: :raspi_unicorn,
+     version: "0.0.1",
+     elixir: "~> 1.1",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:nerves, :logger, :nerves_io_led],
+     mod: {RaspiUnicorn, []}]
+  end
+
+  defp deps, do: [
+    {:nerves, github: "nerves-project/nerves"},
+    {:nerves_io_led, github: "nerves-project/nerves_io_led"}
+  ]
+
+end
