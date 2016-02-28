8c2d12c4c90677da9ca714a15cfbf7bd00763d98
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2016-02-28 17:22:59 +0100

    Add a public API for the Led Service

--- nerves/led_iot/embedded_led/lib/embedded_led/hardware/led.ex
+++ nerves/led_iot/embedded_led/lib/embedded_led/hardware/led.ex
@@ -5,8 +5,34 @@ defmodule EmbeddedLed.Hardware.Led do
 
   require Logger
 
+  ## API
+
+  def led_on(pid, led) do
+    GenServer.cast({:led, pid}, {:on, led})
+  end
+
+  def led_off(pid, led) do
+    GenServer.cast({:led, pid}, {:off, led})
+  end
+
+  def led_list(pid) do
+    GenServer.call({:led, pid}, :led_keys)
+  end
+
+  ## GenServer implementation
+
   def start_link do
     Logger.debug "#{__MODULE__}.start_link"
+
+    Enum.map get_configured_led_list, fn (led_key) ->
+      module_key = "__MODULE__#{led_key}"
+      name = "led_#{led_key}"
+
+      Logger.debug "#{__MODULE__} -> GenServer.start(#{module_key}, [#{led_key}], name: #{name})"
+
+      #GenServer.start(module_key, [led_key], name: name)
+    end
+    #{:ok, self}
 
     GenServer.start_link(__MODULE__, [], name: :led)
   end
@@ -36,11 +62,15 @@ defmodule EmbeddedLed.Hardware.Led do
   end
 
   def handle_call(:led_keys, _from, state) do
-    led_keys = Application.get_env :embedded_led, :led_list
+    led_keys = get_configured_led_list
 
     Logger.debug "reply led_keys: #{inspect(led_keys)}"
 
     {:reply, led_keys, state}
+  end
+
+  defp get_configured_led_list do
+    Application.get_env :embedded_led, :led_list
   end
 
 end
